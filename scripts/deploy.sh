bc79b215b5f73d616a1bcb2845d72ac1d1967b79
Matt Doyle <matt.doyle@zendesk.com>
2022-08-19 18:07:14 +1000

    Add vite config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-git push -f git@github.com:doylem/widget-sounds-test.git main
+git push -f git@github.com:doylem/widget-sounds-test.git main:gh-pages
 
 cd -
